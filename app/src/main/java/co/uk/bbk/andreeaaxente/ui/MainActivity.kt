c71338b1c9c7d9f590cd41e2bc7f449285ff943e
aaxent01 <axenteandreea@hotmail.com>
2025-06-29 13:25:11 +0100

    Added ViewBinding to MainActivity.kt and implemented listeners that will open categories and AddEditRecipeActivity.kt.

--- app/src/main/java/co/uk/bbk/andreeaaxente/ui/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/andreeaaxente/ui/MainActivity.kt
@@ -1,21 +1,38 @@
 package co.uk.bbk.andreeaaxente.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import co.uk.bbk.andreeaaxente.R
+import co.uk.bbk.andreeaaxente.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //accessing the layout views with the help of ViewBinding instance
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // with the help setOnClickListener each respective category is opened on click
+        binding.breakfastButton.setOnClickListener { openCategory("Breakfast") }
+        binding.brunchButton   .setOnClickListener { openCategory("Brunch")   }
+        binding.lunchButton    .setOnClickListener { openCategory("Lunch")    }
+        binding.dinnerButton   .setOnClickListener { openCategory("Dinner")   }
+        binding.dessertsButton .setOnClickListener { openCategory("Desserts") }
+        binding.otherButton    .setOnClickListener { openCategory("Other")    }
+        binding.allRecipesButton.setOnClickListener { openCategory("All")     }
+
+        // setOnClickListener helps open AddEditRecipeActivity when add recipe button is clicked
+        binding.addRecipeButton.setOnClickListener {
+            startActivity(Intent(this, AddEditRecipeActivity::class.java))
+        }
+    }
+    //this function will start CategoryActivity with the selected category
+    private fun openCategory(cat: String) {
+        Intent(this, CategoryActivity::class.java).also {
+            it.putExtra(CategoryActivity.EXTRA_CATEGORY_NAME, cat)
+            startActivity(it)
         }
     }
 }
