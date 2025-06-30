798e122ef54839da06ef338077d29eaa76cc8ae1
aaxent01 <axenteandreea@hotmail.com>
2025-06-30 20:54:24 +0100

    Implemented AppDb.kt and RecipeDao.kt inside data package. First, AppDb.kt implements Room database as AppDatabase that supplies RecipeDao.kt. Secondly, RecipeDao.kt that uses RecipeDao interface ,with CRUD queries, for Recipe entity implementing DAO methods: getAll, getByCategory, getById, insert (with replace), update, delete.

--- app/src/main/java/co/uk/bbk/andreeaaxente/data/AppDb.kt
+++ app/src/main/java/co/uk/bbk/andreeaaxente/data/AppDb.kt
@@ -1,1 +1,30 @@
 package co.uk.bbk.andreeaaxente.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import co.uk.bbk.andreeaaxente.model.Recipe
+
+//this is the Room database class that provides an database instance and its data access object
+@Database(entities = [Recipe::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    //gives access to RecipeDao so that database operations can be performed
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        //retrieves a AppDatabase instance and if needed it will create it, also
+        // avoids activity contexts leaking by using application context
+        fun getInstance(context: Context): AppDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "recipes_db"
+                ).build().also { instance = it }
+            }
+    }
+}
--- app/src/main/java/co/uk/bbk/andreeaaxente/data/RecipeDao.kt
+++ app/src/main/java/co/uk/bbk/andreeaaxente/data/RecipeDao.kt
@@ -1,2 +1,32 @@
 package co.uk.bbk.andreeaaxente.data
 
+import androidx.room.*
+import co.uk.bbk.andreeaaxente.model.Recipe
+
+//data access object interface detailing the operations of the database for Recipe entities
+@Dao
+interface RecipeDao {
+    //get all recipes present in the database
+    @Query("SELECT * FROM recipes")
+    fun getAllRecipes(): List<Recipe>
+
+    //get all the recipes that have the same category
+    @Query("SELECT * FROM recipes WHERE category = :category")
+    fun getRecipesByCategory(category: String): List<Recipe>
+
+    //get a recipe using its unique id
+    @Query("SELECT * FROM recipes WHERE id = :id")
+    fun getRecipeById(id: Int): Recipe?
+
+    //add a recipe to the database and in case of a conflict it will replace it.
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(recipe: Recipe): Long
+
+    //update a present recipe in the database
+    @Update
+    fun update(recipe: Recipe)
+
+    //delete a present recipe in the database
+    @Delete
+    fun delete(recipe: Recipe)
+}
