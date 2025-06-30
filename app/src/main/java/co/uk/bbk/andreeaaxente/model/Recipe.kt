2a95d32773881b66eea83d67d1c4f0063e252762
aaxent01 <axenteandreea@hotmail.com>
2025-06-30 21:59:30 +0100

    Implemented Recipe.kt inside model package adding Recipe data class which implements as Room @Entity containing id, name, cookingTime, ingredients, steps, imageUri, and category.

--- app/src/main/java/co/uk/bbk/andreeaaxente/model/Recipe.kt
+++ app/src/main/java/co/uk/bbk/andreeaaxente/model/Recipe.kt
@@ -1,2 +1,24 @@
 package co.uk.bbk.andreeaaxente.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//this class will store a Recipe entity in the Room database
+@Entity(tableName = "recipes")
+data class Recipe(
+    @PrimaryKey(autoGenerate = true)
+    //this will generate a primary key for a recipe automatically
+    var id: Int = 0,
+    //name of the recipe
+    var name: String,
+    //cooking time in minutes for the recipe
+    var cookingTime: Int,
+    //all recipe ingredients
+    var ingredients: String,
+    //cooking steps
+    var steps: String,
+    //recipe image
+    var imageUri: String? = null,
+    //recipe category
+    var category: String
+)
