c351da44f9261dc7e229fdbee12c0fa9fc29e8ad
aaxent01 <axenteandreea@hotmail.com>
2025-06-30 17:07:49 +0100

    Added RecipeActivity.kt to get the recipe data and display its details on view and the ability to edit and delete any saved recipe.

--- app/src/main/java/co/uk/bbk/andreeaaxente/ui/RecipeActivity.kt
+++ app/src/main/java/co/uk/bbk/andreeaaxente/ui/RecipeActivity.kt
@@ -1,3 +1,72 @@
 package co.uk.bbk.andreeaaxente.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import co.uk.bbk.andreeaaxente.databinding.ActivityRecipePageBinding
+import co.uk.bbk.andreeaaxente.model.Recipe
+import co.uk.bbk.andreeaaxente.repository.RecipeRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import androidx.core.net.toUri
 
+//this class will show details for the chosen recipe and
+// allows actions like edit and delete to be performed
+class RecipePageActivity : AppCompatActivity() {
+    companion object { const val EXTRA_RECIPE_ID = "RECIPE_ID" }
+    private lateinit var binding: ActivityRecipePageBinding
+    private var recipe: Recipe? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecipePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val id = intent.getIntExtra(EXTRA_RECIPE_ID, -1)
+        if (id != -1) {
+            lifecycleScope.launch {
+                recipe = withContext(Dispatchers.IO) {
+                    RecipeRepository.getRecipeById(this@RecipePageActivity, id)
+                }
+                recipe?.let { bindRecipe(it) } ?: finish()
+            }
+        }
+
+        binding.previousPageButton.setOnClickListener { finish() }
+    }
+
+    //this will populate the ui views with the details of the recipe
+    @SuppressLint("SetTextI18n")
+    private fun bindRecipe(r: Recipe) {
+        binding.recipeName.text = r.name
+        binding.cookingTime.text = "${r.cookingTime} min"
+        binding.ingredientsDescription.text = r.ingredients
+        binding.stepsDescription.text = r.steps
+        r.imageUri?.let { binding.recipeImageHeader.setImageURI(it.toUri()) }
+    }
+
+    //the ability to edit the chosen recipe by opening AddEditRecipeActivity
+    fun onEditRecipeClicked(view: View) {
+        recipe?.also {
+            Intent(this, AddEditRecipeActivity::class.java).apply {
+                putExtra(AddEditRecipeActivity.EXTRA_RECIPE_ID, it.id)
+                putExtra(AddEditRecipeActivity.EXTRA_CATEGORY_NAME, it.category)
+                startActivity(this)
+            }
+        }
+    }
+
+    //the ability to delete the chosen recipe and finish the activity
+    fun onDeleteRecipeClicked(view: View) {
+        recipe?.let {
+            lifecycleScope.launch {
+                withContext(Dispatchers.IO) { RecipeRepository.deleteRecipe(this@RecipePageActivity, it) }
+                finish()
+            }
+        }
+    }
+}
